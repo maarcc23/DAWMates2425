e6c8c813e48680ddcf8faf26117891303899c1c9
marc <marcpascual@ceroca.cat>
2024-09-23 19:55:18 +0200

    moviment jugador

---
+++ Assets/Scripts/AvionJugador.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaveJugador : MonoBehaviour
+{
+    private float _vel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _vel = 8f; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direccioIndicadaX = Input.GetAxisRaw("Horizontal");
+        float direccioIndicadaY = Input.GetAxisRaw("Vertical");
+        //Debug.Log("X: " + direccioIndicadaX + " - Y: " + direccioIndicadaY);
+        Vector2 direccioIndicada = new Vector2(direccioIndicadaX, direccioIndicadaY);
+
+        Vector2 novaPos = transform.position; //tranform.position: pos actual nau
+        novaPos = novaPos + direccioIndicada * _vel * Time.deltaTime;
+        //Debug.Log(Time.deltaTime);
+        transform.position = novaPos;
+    }
+}
