3a0a6b2b2ffd924ff8cd778805950d1a09ffd421
marc <marcpascual@ceroca.cat>
2024-09-23 20:24:24 +0200

    limits pantalla

--- Assets/Scripts/AvionJugador.cs
+++ Assets/Scripts/AvionJugador.cs
@@ -6,10 +6,14 @@ public class NaveJugador : MonoBehaviour
 {
     private float _vel;
 
+    private Vector2 minpantalla, maxpantalla;
+
     // Start is called before the first frame update
     void Start()
     {
-        _vel = 8f; 
+        _vel = 8f;
+        minpantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        maxpantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
     }
 
     // Update is called once per frame
@@ -23,6 +27,10 @@ public class NaveJugador : MonoBehaviour
         Vector2 novaPos = transform.position; //tranform.position: pos actual nau
         novaPos = novaPos + direccioIndicada * _vel * Time.deltaTime;
         //Debug.Log(Time.deltaTime);
+
+        novaPos.x = Mathf.Clamp(novaPos.x, minpantalla.x, maxpantalla.x);
+        novaPos.y = Mathf.Clamp(novaPos.y, minpantalla.y, maxpantalla.y);
+
         transform.position = novaPos;
     }
 }
