c2835a3531729e857512e26318fae16a8d6b5c17
marc <marcpascual@ceroca.cat>
2024-09-23 20:03:39 +0200

    Direccio moviment normalitzada

--- Assets/Scripts/AvionJugador.cs
+++ Assets/Scripts/AvionJugador.cs
@@ -18,7 +18,7 @@ public class NaveJugador : MonoBehaviour
         float direccioIndicadaX = Input.GetAxisRaw("Horizontal");
         float direccioIndicadaY = Input.GetAxisRaw("Vertical");
         //Debug.Log("X: " + direccioIndicadaX + " - Y: " + direccioIndicadaY);
-        Vector2 direccioIndicada = new Vector2(direccioIndicadaX, direccioIndicadaY);
+        Vector2 direccioIndicada = new Vector2(direccioIndicadaX, direccioIndicadaY).normalized;
 
         Vector2 novaPos = transform.position; //tranform.position: pos actual nau
         novaPos = novaPos + direccioIndicada * _vel * Time.deltaTime;
